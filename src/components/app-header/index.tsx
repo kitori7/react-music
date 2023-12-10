a6850c9c17d90866ba952ae522b79daeb8b7feba
kitori <kitori@126.com>
2023-12-10 22:56:42 +0800

    ✨ feat(12.10): 第二天第十集

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,19 +1,62 @@
 import { memo } from "react";
 import type { FC, ReactNode } from "react";
-import { Link } from "react-router-dom";
+import { HeaderLeft, HeaderRight, HeaderWrapper } from "./style";
+
+import headerTitles from "@/assets/data/header_titles.json";
+import { NavLink } from "react-router-dom";
+import { SearchOutlined } from "@ant-design/icons";
+import { Input } from "antd";
 
 interface IProps {
   children?: ReactNode;
 }
 
 const AppHeader: FC<IProps> = memo(() => {
+  // 展示item
+  function showItem(item: any) {
+    if (item.type === "path") {
+      return (
+        <NavLink to={item.link}>
+          {item.title} <i className="icon sprite_01"></i>
+        </NavLink>
+      );
+    } else {
+      return (
+        <a href={item.link} target="_blank">
+          {item.title}
+        </a>
+      );
+    }
+  }
   return (
-    <div className="nav">
-      <Link to="/discover">发现音乐</Link>
-      <Link to="/mine">我的音乐</Link>
-      <Link to="/focus">关注</Link>
-      <Link to="/download">下载客户端</Link>
-    </div>
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="/">
+            网易云音乐
+          </a>
+          <div className="title-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              );
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <Input
+            className="search"
+            placeholder="音乐/视频/电台/用户"
+            prefix={<SearchOutlined />}
+          ></Input>
+          <span className="center">创作者中心</span>
+          <span className="login">登录</span>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
 
